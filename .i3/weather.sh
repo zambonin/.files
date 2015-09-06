e97b58b34af391f7e212815f745c3b1afa529cd5
Gustavo Zambonin <gzmbnn@gmail.com>
2015-09-05 23:00:51 -0300

    i3: deleted i3status configuration file and fixed two hiccups

--- .i3/weather.sh
+++ .i3/weather.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 link="http://weather.noaa.gov/weather/current/SBFL.html"
-temp=$(curl -s $link | grep --context=2 "Temperature" $weather | sed '5q;d' | awk '{print $5}' | sed 's/(//')
+temp=$(curl -s $link | grep --context=2 "Temperature" | sed '5q;d' | awk '{print $5}' | sed 's/(//')
 cond=$(curl -s $link | grep --context=2 "Sky conditions" | sed '5q;d' | awk '{print $3 " " $4}')
 echo $temp°C, $cond

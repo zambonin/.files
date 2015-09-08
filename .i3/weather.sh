4d1d9d1482b00ef00da47b58c0ddaa7d7de3cb5f
Gustavo Zambonin <gzmbnn@gmail.com>
2015-09-08 18:56:24 -0300

    conky: standardize information on the bar

--- .i3/weather.sh
+++ .i3/weather.sh
@@ -4,4 +4,4 @@ sleep 30
 link="http://weather.noaa.gov/weather/current/SBFL.html"
 temp=$(curl -s $link | grep --context=2 "Temperature" | sed '5q;d' | awk '{print $5}' | sed 's/(//')
 cond=$(curl -s $link | grep --context=2 "Sky conditions" | sed '5q;d' | awk '{print $3 " " $4}')
-echo $temp°C, $cond
+echo $cond [$temp°C]

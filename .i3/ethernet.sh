a9d334f15561f9bc2085f6240b8f764615184ebd
Gustavo Zambonin <gzmbnn@gmail.com>
2015-10-22 14:33:20 -0200

    i3blocks: less visual pollution on ethernet script

--- .i3/ethernet.sh
+++ .i3/ethernet.sh
@@ -2,6 +2,5 @@
 
 interface="enp0s20u3"
 
-ip=$(ip addr | awk 'FNR==15 {print $2}')
-[[ "$(cat /sys/class/net/$interface/operstate)" ]] && echo "$interface: $ip"
+[[ "$(cat /sys/class/net/$interface/operstate)" ]] && echo "$interface [✓]"
 
